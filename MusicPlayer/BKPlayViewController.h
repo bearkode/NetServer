58393d1b8f2a273c0c1e05f9235f9538f6f4ff53
Chang-Gi, Kim <cgkim@mac.com>
2015-05-06 20:53:08 +0900

    add BKPlayViewController

---
+++ MusicPlayer/BKPlayViewController.h
@@ -0,0 +1,16 @@
+/*
+ *  BKPlayViewController.h
+ *  NetServer
+ *
+ *  Created by bearkode on 2015. 5. 6..
+ *  Copyright (c) 2015 bearkode. All rights reserved.
+ *
+ */
+
+#import <UIKit/UIKit.h>
+
+
+@interface BKPlayViewController : UIViewController
+
+
+@end
---
+++ MusicPlayer/BKPlayViewController.m
@@ -0,0 +1,28 @@
+/*
+ *  BKPlayViewController.m
+ *  NetServer
+ *
+ *  Created by bearkode on 2015. 5. 6..
+ *  Copyright (c) 2015 bearkode. All rights reserved.
+ *
+ */
+
+#import "BKPlayViewController.h"
+
+
+@implementation BKPlayViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
